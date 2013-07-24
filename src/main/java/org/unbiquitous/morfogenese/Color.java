306f99d01bb3971f99f4eeaa8ac9f1a00b218fec
Fabricio Buzeto <nuk.anime13@gmail.com>
2013-07-24 17:34:00 -0400

    refactor color in the DNA

---
+++ src/main/java/org/unbiquitous/morfogenese/Color.java
@@ -0,0 +1,41 @@
+package org.unbiquitous.morfogenese;
+
+import processing.core.PApplet;
+
+public class Color {
+
+	private int color;
+	private PApplet p;
+	
+	public Color(PApplet parent, int color) {
+		this.p = parent;
+		this.color = color;
+	}
+	
+	public int color(){
+		return color;
+	}
+	
+	public float red(){
+		return p.red(color);
+	}
+	
+	public float green(){
+		return p.green(color);
+	}
+	
+	public float blue(){
+		return p.blue(color);
+	}
+	
+	public Color set(float r, float g, float b){
+		this.color = p.color(r,g,b);
+		return this;
+	}
+	
+	public static Color color(PApplet parent,float r, float g, float b){
+		return new Color(parent, 0).set(r, g, b);
+	}
+	
+	
+}
